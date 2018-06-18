01d3d1f33bce2b799b39a2715ffb2bbe27af0042
keionbis <keion96@gmail.com>
2018-06-18 19:40:09 -0400

    Removed un necessary files

--- src/main/java/com/FieldSimulator/FieldSimulator_2001/InterfaceController.java
+++ src/main/java/com/FieldSimulator/FieldSimulator_2001/InterfaceController.java
@@ -57,7 +57,7 @@ public class InterfaceController extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/test.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainScreen.fxml"));
         primaryStage.setTitle("Field Simulator");
         root.getStylesheets().add("/materialfx-material-design-for-javafx/material-fx-v0_3.css");
         primaryStage.setScene(new Scene(root, 1000, 750));
