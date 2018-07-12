edcd7256ad792b03c2e2b557b7f4be4de51cdf4e
Kevin Harrington <mad.hephaestus@gmail.com>
2018-07-11 22:16:13 -0400

    Working basic commands

--- src/main/java/com/FieldSimulator/FieldSimulator_2001/InterfaceController.java
+++ src/main/java/com/FieldSimulator/FieldSimulator_2001/InterfaceController.java
@@ -1,98 +1,169 @@
 package com.FieldSimulator.FieldSimulator_2001;
 
-import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.stage.Stage;
 import edu.wpi.SimplePacketComs.device.warehouse.WarehouseRobot;
 
-import java.io.IOException;
+import javafx.event.ActionEvent;
 
+public class InterfaceController {
 
-public class InterfaceController extends Application {
+	WarehouseRobot fieldSim = null;
+	ObservableList<String> Weights = FXCollections.observableArrayList("Aluminum", "Plastic");
+	ObservableList<String> Sides = FXCollections.observableArrayList("25", "45");
+	ObservableList<String> Pos = FXCollections.observableArrayList("1", "2");
+	@FXML
+	private Button connectToDevice;
 
-    WarehouseRobot fieldSim = null;
-    ObservableList<String> Weights = FXCollections.observableArrayList("Aluminum", "Plastic");
-    ObservableList<String> Sides = FXCollections.observableArrayList("25", "45");
-    ObservableList<String> Pos = FXCollections.observableArrayList("1", "2");
-    @FXML
-    Button connect = new Button();
-    @FXML
-    ChoiceBox<String> choiceBoxWeight = new ChoiceBox<>();
-    @FXML
-    ChoiceBox<String> choiceBoxSide = new ChoiceBox<>();
-    @FXML
-    ChoiceBox<String> choiceBoxPos = new ChoiceBox<>();
-    @FXML
-    TextField teamName;
-    @FXML
-    RadioButton heartBeat;
-    @FXML
-    TextArea response;
+	@FXML
+	private Button start;
 
-    public static void main(String[] args) throws IOException {
-        launch(args);
-    }
+	@FXML
+	private Button stop;
 
-    @FXML
-    private void initialize() {
-        choiceBoxWeight.setValue("Aluminum");
-        choiceBoxWeight.setItems(Weights);
-        choiceBoxSide.setValue("25");
-        choiceBoxSide.setItems(Sides);
-        choiceBoxPos.setValue("1");
-        choiceBoxPos.setItems(Pos);
+	@FXML
+	private Button PLE;
 
-        choiceBoxWeight.getSelectionModel().select("Heavy");
-    }
+	@FXML
+	private Button RHE;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+	@FXML
+	private TextArea response;
 
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainScreen.fxml"));
-        primaryStage.setTitle("Field Simulator");
-        root.getStylesheets().add("/materialfx-material-design-for-javafx/material-fx-v0_3.css");
-        primaryStage.setScene(new Scene(root, 1000, 750));
+	@FXML
+	private Button send;
 
-        primaryStage.show();
-        primaryStage.getScene().setRoot(root);
+	@FXML
+	private RadioButton heartBeat;
 
-    }
+	@FXML
+	private ChoiceBox<String> choiceBoxWeight;
 
-    public InterfaceController() throws Exception {
+	@FXML
+	private ChoiceBox<String> choiceBoxSide;
 
-    }
+	@FXML
+	private ChoiceBox<String> choiceBoxPos;
+	@FXML
+	private TextField teamName;
 
-    public void connectToDevice() throws Exception {
+	@FXML
+	private void initialize() {
+		assert connectToDevice != null : "fx:id=\"connectToDevice\" was not injected: check your FXML file 'MainScreen.fxml'.";
+		assert start != null : "fx:id=\"start\" was not injected: check your FXML file 'MainScreen.fxml'.";
+		assert stop != null : "fx:id=\"stop\" was not injected: check your FXML file 'MainScreen.fxml'.";
+		assert PLE != null : "fx:id=\"PLE\" was not injected: check your FXML file 'MainScreen.fxml'.";
+		assert RHE != null : "fx:id=\"RHE\" was not injected: check your FXML file 'MainScreen.fxml'.";
+		assert teamName != null : "fx:id=\"teamName\" was not injected: check your FXML file 'MainScreen.fxml'.";
+		assert response != null : "fx:id=\"response\" was not injected: check your FXML file 'MainScreen.fxml'.";
+		assert send != null : "fx:id=\"send\" was not injected: check your FXML file 'MainScreen.fxml'.";
+		assert heartBeat != null : "fx:id=\"heartBeat\" was not injected: check your FXML file 'MainScreen.fxml'.";
+		assert choiceBoxWeight != null : "fx:id=\"choiceBoxWeight\" was not injected: check your FXML file 'MainScreen.fxml'.";
+		assert choiceBoxSide != null : "fx:id=\"choiceBoxSide\" was not injected: check your FXML file 'MainScreen.fxml'.";
+		assert choiceBoxPos != null : "fx:id=\"choiceBoxPos\" was not injected: check your FXML file 'MainScreen.fxml'.";
 
-        fieldSim = WarehouseRobot.get(teamName.getText()).get(0);
-        fieldSim.addEvent(Integer.valueOf(1936), ()->{
+		choiceBoxWeight.setValue(Weights.get(0));
+		choiceBoxWeight.setItems(Weights);
+		choiceBoxSide.setValue("25");
+		choiceBoxSide.setItems(Sides);
+		choiceBoxPos.setValue("1");
+		choiceBoxPos.setItems(Pos);
 
-        });
+		choiceBoxWeight.getSelectionModel().select(Weights.get(0));
 
-    }
+		start.setDisable(true);
+		stop.setDisable(true);
+		PLE.setDisable(true);
+		RHE.setDisable(true);
+		send.setDisable(true);
 
-    public void sendLocation(){
+	}
 
+	public InterfaceController() throws Exception {
 
-    }
-    public void sendRaisedHigh(){
+	}
 
-    }
-    public void sendLowered(){
+	public void connectToDevice() {
+		if (fieldSim == null) {
+			connectToDevice.setDisable(true);
+			new Thread(() -> {
+				try {
+					fieldSim = WarehouseRobot.get(teamName.getText()).get(0);
+					if (fieldSim != null) {
+						fieldSim.addEvent(Integer.valueOf(1936), () -> {
 
-    }
-    public void start(){
+						});
 
-    }
-    public void stop(){
+						Platform.runLater(() -> {
+							start.setDisable(false);
+							stop.setDisable(false);
+							PLE.setDisable(false);
+							RHE.setDisable(false);
+							send.setDisable(false);
+						});
+					}
+				} catch (Throwable e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				if (fieldSim == null)
+					Platform.runLater(() -> {
+						connectToDevice.setDisable(false);
+					});
+			}).start();
+		}
+	}
 
-    }
+	@FXML
+	void sendLocation(ActionEvent event) {
+		System.out.println("sendLocation");
+		try {
+			double material =0;
+			if(choiceBoxWeight.getSelectionModel().getSelectedItem().contains(Weights.get(0))) {
+				material=1;
+			}else {
+				material=2;
+			}
+			double angle = Double.parseDouble(choiceBoxSide.getSelectionModel().getSelectedItem());
+			double position = Double.parseDouble(choiceBoxPos.getSelectionModel().getSelectedItem());
+			if (fieldSim != null)
+				fieldSim.pickOrder(material, angle, position);
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
 
+	}
 
+	@FXML
+	void sendLowered(ActionEvent event) {
+		System.out.println("sendLowered");
+	}
+
+	@FXML
+	void sendRaisedHigh(ActionEvent event) {
+		System.out.println("sendRaisedHigh");
+	}
+
+	@FXML
+	void start(ActionEvent event) {
+		System.out.println("start");
+		if (fieldSim != null)
+			fieldSim.clearFaults();
+	}
+
+	@FXML
+	void stop(ActionEvent event) {
+		System.out.println("stop");
+		if (fieldSim != null)
+			fieldSim.estop();
+	}
+
+	@FXML
+	void onConnect(ActionEvent event) {
+		System.out.println("onConnect");
+		connectToDevice();
+	}
 }
---
+++ src/main/java/com/FieldSimulator/FieldSimulator_2001/Main.java
@@ -0,0 +1,27 @@
+package com.FieldSimulator.FieldSimulator_2001;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main  extends Application {
+
+	public static void main(String[] args) throws IOException {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainScreen.fxml"));
+		primaryStage.setTitle("Field Simulator");
+		root.getStylesheets().add("/materialfx-material-design-for-javafx/material-fx-v0_3.css");
+		primaryStage.setScene(new Scene(root, 1100, 850));
+
+		primaryStage.show();
+		primaryStage.getScene().setRoot(root);
+	}
+
+}
