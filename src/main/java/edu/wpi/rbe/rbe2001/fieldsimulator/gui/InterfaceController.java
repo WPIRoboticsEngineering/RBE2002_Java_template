f4b074c4184caa6271ca530bf4a066022fc1f9fa
Kevin Harrington <mad.hephaestus@gmail.com>
2019-01-09 20:26:34 -0500

    fixing walking Setpoint index

--- src/main/java/edu/wpi/rbe/rbe2001/fieldsimulator/gui/InterfaceController.java
+++ src/main/java/edu/wpi/rbe/rbe2001/fieldsimulator/gui/InterfaceController.java
@@ -20,82 +20,77 @@ import javafx.scene.chart.XYChart.Series;
 
 public class InterfaceController {
 
-    @FXML
-    private Tab connectTab;
-
-    @FXML
-    private TextField teamName;
-
-    @FXML
-    private Button connectToDevice;
-
-    @FXML
-    private Label robotName;
-
-    @FXML
-    private Tab pidVelTab;
-
-
-    @FXML
-    private TextField kpVel;
-
-    @FXML
-    private TextField kdVel;
-
-    @FXML
-    private Button pidConstUpdateVelocity;
-
-    @FXML
-    private ChoiceBox<?> pidChannelVelocity;
-
-    @FXML
-    private TextField setpointVelocity;
-
-    @FXML
-    private Button setSetpointVelocity;
-
-    @FXML
-    private Label velocityVal;
-
-    @FXML
-    private Tab pidTab;
-
-
-    @FXML
-    private TextField kp;
-
-    @FXML
-    private TextField ki;
-
-    @FXML
-    private TextField kd;
-
-    @FXML
-    private Button pidConstUpdate;
-
-    
-
-    @FXML
-    private TextField setpoint;
-
-    @FXML
-    private Button setSetpoint;
-
-    @FXML
-    private Label position;
-    
-    @FXML
-    private ChoiceBox<Integer> pidChannel;
-
-    @FXML // fx:id="setDuration"
-    private TextField setDuration; // Value injected by FXMLLoader
-
-    @FXML // fx:id="setType"
-    private ChoiceBox<String> setType; // Value injected by FXMLLoader
-    
-    
 	@FXML
-	private LineChart<Double, Double> pidGraph;	
+	private Tab connectTab;
+
+	@FXML
+	private TextField teamName;
+
+	@FXML
+	private Button connectToDevice;
+
+	@FXML
+	private Label robotName;
+
+	@FXML
+	private Tab pidVelTab;
+
+	@FXML
+	private TextField kpVel;
+
+	@FXML
+	private TextField kdVel;
+
+	@FXML
+	private Button pidConstUpdateVelocity;
+
+	@FXML
+	private ChoiceBox<?> pidChannelVelocity;
+
+	@FXML
+	private TextField setpointVelocity;
+
+	@FXML
+	private Button setSetpointVelocity;
+
+	@FXML
+	private Label velocityVal;
+
+	@FXML
+	private Tab pidTab;
+
+	@FXML
+	private TextField kp;
+
+	@FXML
+	private TextField ki;
+
+	@FXML
+	private TextField kd;
+
+	@FXML
+	private Button pidConstUpdate;
+
+	@FXML
+	private TextField setpoint;
+
+	@FXML
+	private Button setSetpoint;
+
+	@FXML
+	private Label position;
+
+	@FXML
+	private ChoiceBox<Integer> pidChannel;
+
+	@FXML // fx:id="setDuration"
+	private TextField setDuration; // Value injected by FXMLLoader
+
+	@FXML // fx:id="setType"
+	private ChoiceBox<String> setType; // Value injected by FXMLLoader
+
+	@FXML
+	private LineChart<Double, Double> pidGraph;
 	@FXML
 	private LineChart<Double, Double> pidGraphVel;
 	private ArrayList<XYChart.Series> pidGraphSeriesVel = new ArrayList<>();
@@ -103,15 +98,17 @@ public class InterfaceController {
 
 	private double piddata[] = null;
 	private double pidConfig[] = null;
+	private double currentSetpoint[] = null;
 	private DecimalFormat formatter = new DecimalFormat();
-	private double start  =((double)System.currentTimeMillis())/1000.0;
+	private double start = ((double) System.currentTimeMillis()) / 1000.0;
 	private long lastPos;
 	private long lastSet;
 	static InterfaceController me;
 	private static RBE2001Robot fieldSim;
 	private int numPIDControllers = 0;
-	private int currentIndex=0;
-	private static final int numPIDControllersOnDevice=3;
+	private int currentIndex = 0;
+	private static final int numPIDControllersOnDevice = 3;
+
 	@FXML
 	private void initialize() {
 		me = this;
@@ -133,18 +130,17 @@ public class InterfaceController {
 		assert position != null : "fx:id=\"position\" was not injected: check your FXML file 'MainScreen.fxml'.";
 		teamName.setText("IMU-Team21");
 
-		
-		for(int i=0;i<numPIDControllersOnDevice;i++) {
+		for (int i = 0; i < numPIDControllersOnDevice; i++) {
 			Series e = new XYChart.Series();
-			
-			pidGraphSeries.add(i,e);
+
+			pidGraphSeries.add(i, e);
 			pidGraph.getData().add(e);
 		}
 		pidGraphVel.getXAxis().autoRangingProperty().set(true);
-		for(int i=0;i<numPIDControllersOnDevice;i++) {
+		for (int i = 0; i < numPIDControllersOnDevice; i++) {
 			Series e = new XYChart.Series();
-			
-			pidGraphSeriesVel.add(i,e);
+
+			pidGraphSeriesVel.add(i, e);
 			pidGraphVel.getData().add(e);
 		}
 		pidGraphVel.getXAxis().autoRangingProperty().set(true);
@@ -187,24 +183,24 @@ public class InterfaceController {
 		double kpv = Double.parseDouble(kp.getText());
 		double kiv = Double.parseDouble(ki.getText());
 		double kdv = Double.parseDouble(kd.getText());
-		for(int i=0;i<numPIDControllers;i++)
+		for (int i = 0; i < numPIDControllers; i++)
 			fieldSim.setPidGains(i, kpv, kiv, kdv);
 	}
 
 	@FXML
 	void onSetSetpoint() {
-		double down[] = new double [2+2*numPIDControllersOnDevice];
+		double down[] = new double[2 + 2 * numPIDControllersOnDevice];
 		down[0] = Integer.parseInt(setDuration.getText());
-		down[1] = setType.getSelectionModel().getSelectedItem().equals("LIN")?0:1;
-		for(int i=0;i<numPIDControllersOnDevice;i++) {
-			if(i==currentIndex) {
-				down[2+i] = Double.parseDouble(setpoint.getText());
-			}else {
-				down[2+i] =piddata[1+currentIndex*2+0];
+		down[1] = setType.getSelectionModel().getSelectedItem().equals("LIN") ? 0 : 1;
+		for (int i = 0; i < numPIDControllersOnDevice; i++) {
+			if (i == currentIndex) {
+				down[2 + i] = Double.parseDouble(setpoint.getText());
+			} else {
+				down[2 + i] = currentSetpoint[i];
 			}
 		}
 		fieldSim.setPidSetpoints(down);
-		
+
 	}
 
 	public RBE2001Robot getRobot() {
@@ -217,41 +213,47 @@ public class InterfaceController {
 
 		fieldSim.addEvent(1910, () -> {
 			try {
-				if (piddata == null)
-					piddata = new double[1+2*numPIDControllersOnDevice];
+				if (piddata == null) {
+					piddata = new double[1 + 2 * numPIDControllersOnDevice];
+					currentSetpoint = new double[numPIDControllersOnDevice];
+				}
 				fieldSim.readFloats(1910, piddata);
 				int myNumPid = (int) piddata[0];
 				if (numPIDControllers != myNumPid) {
-					numPIDControllers=myNumPid;
+					numPIDControllers = myNumPid;
 					setUpPid();
 				}
-				double pos = piddata[1+currentIndex*2+1];
-				double set = piddata[1+currentIndex*2+0];
+				double pos = piddata[1 + currentIndex * 2 + 1];
+				double set = piddata[1 + currentIndex * 2 + 0];
+				for(int i=0;i<numPIDControllersOnDevice;i++) {
+					currentSetpoint[i]=piddata[1 + i * 2 + 0];
+				}
 				String positionVal = formatter.format(pos);
-				//System.out.println(positionVal+" "+DoubleStream.of(piddata).boxed().collect(Collectors.toCollection(ArrayList::new)));
-;
-				Platform.runLater(() ->position.setText(positionVal ));
-				Platform.runLater(() ->updateGraph(pos, set));
-				
+				// System.out.println(positionVal+"
+				// "+DoubleStream.of(piddata).boxed().collect(Collectors.toCollection(ArrayList::new)));
+				;
+				Platform.runLater(() -> position.setText(positionVal));
+				Platform.runLater(() -> updateGraph(pos, set));
+
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
 		});
-		
 
 		fieldSim.addEvent(1857, () -> {
 			try {
 				if (pidConfig == null)
-					pidConfig = new double[3*numPIDControllersOnDevice];
+					pidConfig = new double[3 * numPIDControllersOnDevice];
 				fieldSim.readFloats(1857, pidConfig);
-				
-				//System.out.println(" "+DoubleStream.of(pidConfig).boxed().collect(Collectors.toCollection(ArrayList::new)));
 
-				Platform.runLater(() ->kp.setText(formatter.format(pidConfig[currentIndex*3+0]) ));
-				
-				Platform.runLater(() ->ki.setText(formatter.format(pidConfig[currentIndex*3+1]) ));
+				// System.out.println("
+				// "+DoubleStream.of(pidConfig).boxed().collect(Collectors.toCollection(ArrayList::new)));
 
-				Platform.runLater(() ->kd.setText(formatter.format(pidConfig[currentIndex*3+2]) ));
+				Platform.runLater(() -> kp.setText(formatter.format(pidConfig[currentIndex * 3 + 0])));
+
+				Platform.runLater(() -> ki.setText(formatter.format(pidConfig[currentIndex * 3 + 1])));
+
+				Platform.runLater(() -> kd.setText(formatter.format(pidConfig[currentIndex * 3 + 2])));
 
 			} catch (Exception ex) {
 				ex.printStackTrace();
@@ -260,25 +262,26 @@ public class InterfaceController {
 		fieldSim.updatConfig();
 
 	}
+
 	@SuppressWarnings("unchecked")
-	private void updateGraph(double pos,double set) {
-		if(pidGraphSeries.size()==0)
+	private void updateGraph(double pos, double set) {
+		if (pidGraphSeries.size() == 0)
 			return;
-		double now  =((double)System.currentTimeMillis())/1000.0-start;
+		double now = ((double) System.currentTimeMillis()) / 1000.0 - start;
 		long thispos = (long) pos;
-		long thisSet=(long) set;
-		if(thispos != lastPos || thisSet!=lastSet) {
-			pidGraphSeries.get(0).getData().add(new XYChart.Data( now-0.0001, lastPos));
-			pidGraphSeries.get(1).getData().add(new XYChart.Data( now-0.0001, lastSet));
-			lastSet=thisSet;
-			lastPos=thispos;
-			pidGraphSeries.get(0).getData().add(new XYChart.Data( now, pos));
-			pidGraphSeries.get(1).getData().add(new XYChart.Data( now, set));
+		long thisSet = (long) set;
+		if (thispos != lastPos || thisSet != lastSet) {
+			pidGraphSeries.get(0).getData().add(new XYChart.Data(now - 0.0001, lastPos));
+			pidGraphSeries.get(1).getData().add(new XYChart.Data(now - 0.0001, lastSet));
+			lastSet = thisSet;
+			lastPos = thispos;
+			pidGraphSeries.get(0).getData().add(new XYChart.Data(now, pos));
+			pidGraphSeries.get(1).getData().add(new XYChart.Data(now, set));
 		}
-		for(Series s:pidGraphSeries) {		
-			while(s.getData().size()>200) {
+		for (Series s : pidGraphSeries) {
+			while (s.getData().size() > 200) {
 				s.getData().remove(0);
-			}			
+			}
 		}
 	}
 
@@ -289,19 +292,19 @@ public class InterfaceController {
 				int index = i;
 				Platform.runLater(() -> pidChannel.getItems().add(index));
 			}
-			pidChannel.getSelectionModel().selectedIndexProperty().addListener((obs,old,newVal)->{
-				System.out.println("Set to channel "+newVal);
-				currentIndex=newVal.intValue();
+			pidChannel.getSelectionModel().selectedIndexProperty().addListener((obs, old, newVal) -> {
+				System.out.println("Set to channel " + newVal);
+				currentIndex = newVal.intValue();
 				fieldSim.updatConfig();
-				for(Series s:pidGraphSeries) {		
-						s.getData().clear();
-								
+				for (Series s : pidGraphSeries) {
+					s.getData().clear();
+
 				}
 			});
-			Platform.runLater(() ->pidChannel.setValue(0));
+			Platform.runLater(() -> pidChannel.setValue(0));
 			Platform.runLater(() -> setType.getItems().add("LIN"));
 			Platform.runLater(() -> setType.getItems().add("SIN"));
-			Platform.runLater(() ->setType.setValue("LIN"));
+			Platform.runLater(() -> setType.setValue("LIN"));
 		}
 	}
 
@@ -309,13 +312,15 @@ public class InterfaceController {
 		if (me.getRobot() != null)
 			me.getRobot().disconnect();
 	}
-    @FXML
-    void onSetVelocity() {
 
-    }
-    @FXML
-    void onSetGainsVelocity() {
+	@FXML
+	void onSetVelocity() {
 
-    }
-    
+	}
+
+	@FXML
+	void onSetGainsVelocity() {
+
+	}
+
 }
